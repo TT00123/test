b0e45d289711d5d707aa5e65ebdac892dd2e6a54
itsqt <854027671@qq.com>
2023-04-23 21:24:29 +0800

    新增

---
+++ js/chat.js
@@ -0,0 +1,22 @@
+// resetui 重置滚动条的位置
+$(function(){
+    // 初始化右侧滚动条
+    // 这个方法定义在scroll.js中
+    resetui()
+
+
+    // 为发送按钮绑定鼠标点击事件
+    $('#btnSend').on('click',function () {
+        //trim 清空字符串两端空格
+        var text = $('#ipt').val().trim()
+        if (text.length <= 0) {
+            return $('#ipt').val('')
+         
+        }
+    // 如果用户输入了聊天内容，则将聊天内容追加到页面上显示
+        $('#talk_list').append('<li class="right_word"><img src="img/person02.png" /> <span>'+text+'</span></li>')
+        $('#ipt').val('')
+        resetui()
+    })
+})
+  
